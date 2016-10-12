8acf163f6158e5617ffdb1de46009d6904e2e8d5
plx927 <784580872@qq.com>
2016-10-12 18:36:32 +0800

    学习Activemq的producer-flow-control

--- ActiveMQ-learning-demo-producer/src/main/java/com/panlingxiao/activemq/learning/demo/producer/MyTopicProducer.java
+++ ActiveMQ-learning-demo-producer/src/main/java/com/panlingxiao/activemq/learning/demo/producer/MyTopicProducer.java
@@ -13,19 +13,23 @@ public class MyTopicProducer {
         ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("admin", "password", "tcp://172.16.1.86:61616");
         Connection connection = connectionFactory.createConnection();
         connection.start();
-        Session session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
         Topic topic = session.createTopic("test-topic");
         MessageProducer producer = session.createProducer(null);
 
         try {
-            for (int i = 0; i < 1000000; i++) {
+            for (int i = 0; i < 100000; i++) {
                 MapMessage mapMessage = session.createMapMessage();
                 mapMessage.setInt("id", i);
                 mapMessage.setString("text", "hello:" + i);
                 producer.send(topic, mapMessage);
                 System.out.printf("发送消息[id:%d,text:%s]到主题\n", mapMessage.getInt("id"), mapMessage.getString("text"));
+//                if(i > 0 && (i % 100000) == 0){
+//                    session.commit();
+//                }
             }
-          //  session.commit();
+           // session.commit();
+
         } catch (JMSException e) {
             e.printStackTrace();
         }finally {
